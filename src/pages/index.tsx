f9b10dc4e66d2e7d7b7fd1fe07357bf99d64cca7
Giacomo Rebonato <grebonato@outlook.ie>
2020-03-28 01:13:32 +0000

    First changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,109 +1,88 @@
-import React, { ReactElement, useState } from "react"
+import React from 'react'
 
-interface Props {}
+const Title: React.FC = ({ children }) => (
+  <h1 className='text-xl font-bold text-gray-800'>{children}</h1>
+)
 
-function Index({}: Props): ReactElement {
-  const [isMenuOpen, setMenuOpen] = useState(false)
+const Chapter: React.FC = ({ children }) => (
+  <h2 className='font-semibold mt-4 text-xl mb-1'>{children}</h2>
+)
+
+const P: React.FC = ({ children }) => (
+  <p className='text-lg text-gray-800 mb-2'>{children}</p>
+)
+
+interface LinkProps {
+  href: string
+}
+
+const Link: React.FC<LinkProps> = ({ children, href }) => (
+  <a
+    className='text-blue-700 hover:text-blue-800'
+    target='_blank'
+    href={href}
+    rel='noopener noreferrer'
+  >
+    {children}
+  </a>
+)
+
+const Index: React.FC = () => {
   return (
-    <>
-      <div>
-        <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-          <div className="flex items-center flex-shrink-0 text-white mr-6">
-            <svg
-              className="fill-current h-8 w-8 mr-2"
-              width="54"
-              height="54"
-              viewBox="0 0 54 54"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-            </svg>
-            <span className="font-semibold text-xl tracking-tight">
-              Gatsby TypeScript Tailwind CSS
-            </span>
-          </div>
-          <div className="block lg:hidden">
-            <button
-              onClick={() => {
-                setMenuOpen(isMenuOpen => !isMenuOpen)
-              }}
-              className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
-            >
-              <svg
-                className="fill-current h-3 w-3"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <title>Menu</title>
-                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-              </svg>
-            </button>
-          </div>
-          <div
-            className={`w-full ${
-              isMenuOpen ? "block" : "hidden"
-            } lg:block flex-grow lg:flex lg:items-center lg:w-auto`}
-          >
-            <div className="text-sm lg:flex-grow">
-              <a
-                href="#responsive-header"
-                className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-              >
-                Docs
-              </a>
-              <a
-                href="#responsive-header"
-                className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-              >
-                Examples
-              </a>
-              <a
-                href="#responsive-header"
-                className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-              >
-                Blog
-              </a>
-            </div>
-            <div>
-              <a
-                href="/"
-                className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-              >
-                Download
-              </a>
-            </div>
-          </div>
-        </nav>
-      </div>
-      <div className="container max-w-md mx-auto mt-10">
-        <div className="rounded overflow-hidden shadow-lg">
+    <div
+      className='flex font-sans flex-col p-4 border-gray-600 border-l border-r'
+      style={{ margin: '0 auto', maxWidth: '42rem' }}
+    >
+      <header>
+        <Title>Giacomo Rebonato</Title>
+        <div className='flex justify-center'>
           <img
-            className="w-full"
-            src="https://tailwindcss.com/img/card-top.jpg"
-            alt="Sunset in the mountains"
+            className='border border-4 rounded-full'
+            src='/images/avatar.jpg'
+            alt='Sunset in the mountains'
+            width='250'
           />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-            <p className="text-gray-700 text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
-            </p>
-          </div>
-          <div className="px-6 py-4">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-              #photography
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-              #travel
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-              #winter
-            </span>
-          </div>
         </div>
-      </div>
-    </>
+      </header>
+      <Chapter>Who am I?</Chapter>
+      <P>
+        I am a programmer and a musician based in Dublin Ireland.
+        <br />
+        Passionate about:
+      </P>
+      <ul className='list-disc list-inside'>
+        <li>ReactJS</li>
+        <li>A11Y</li>
+        <li>D3</li>
+        <li>NodeJS</li>
+        <li>TypeScript</li>
+        <li>GraphQL</li>
+        <li>Testing</li>
+      </ul>
+      <Chapter>Other things</Chapter>
+      <ul className='list-disc list-inside'>
+        <li>I like movies</li>
+        <li>Playing music</li>
+        <li>People</li>
+      </ul>
+      <Chapter>Contacts</Chapter>
+      <ul className='list-disc list-inside'>
+        <li>
+          <Link href='https://github.com/giacomorebonato/'>Github</Link>
+        </li>
+        <li>
+          <Link href='https://twitter.com/giacomorebonato'>Twitter</Link>
+        </li>
+        <li>
+          <Link href='https://www.linkedin.com/in/giacomorebonato/'>
+            Linkedin
+          </Link>
+        </li>
+        <li>
+          <Link href='mailto:grebonato@outlook.ie'>Email</Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 

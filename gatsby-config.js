aac6e6d1cbd385cc568f3d0a8f5b53d1e39a5164
Giacomo Rebonato <grebonato@outlook.ie>
2020-03-29 14:52:26 +0100

    Enabling Gatsby PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,9 +46,12 @@ module.exports = {
           failOnError: false
         }
       }
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: ['/'],
+      },
     }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
   ],
 }

144705789909f02fe17d1f070b015b84a333f39e
Giacomo Rebonato <grebonato@outlook.ie>
2020-12-25 20:05:18 +0100

    Revert "Check if this improves initial render"
    This reverts commit b1d0fb25ef2900f7a27f5691a7641184f06cabb5.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { extractCritical } from '@emotion/server'
-
 import 'twin.macro'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
-    const page = await ctx.renderPage()
-    const styles = extractCritical(page.html)
-
-    return { ...initialProps, ...page, ...styles }
+    return { ...initialProps }
   }
 
   render() {

80bce54a84bc674f24d36d72011fb440f974769b
Giacomo Rebonato <grebonato@outlook.ie>
2021-03-17 14:13:44 +0100

    Adjusted max width to look good on tablet.

--- components/index.tsx
+++ components/index.tsx
@@ -1,35 +1,8 @@
 import NextLink from 'next/link'
 import React from 'react'
-import { GlobalStyles } from 'twin.macro'
-import { Toggle } from './Toggle'
-import { useDarkMode } from './useDarkMode'
-
-export const Container: React.FC<any> = ({ css, children }) => {
-  const [isDark, setDarkMode] = useDarkMode()
-
-  return (
-    <main
-      tw='relative container mx-auto rounded-sm flex font-sans flex-col p-4 mb-4 lg:border-l lg:border-r border-gray-600 min-h-screen'
-      style={{ maxWidth: '42rem', margin: '0 auto' }}
-    >
-      <GlobalStyles />
-      <header tw='mb-4'>
-        <Title>
-          <NextLink href='/'>Giacomo Rebonato</NextLink>
-        </Title>
-        <Toggle
-          checked={isDark}
-          name='dark-mode'
-          text='Dark mode'
-          onChange={(e) => {
-            setDarkMode(e.target.checked)
-          }}
-        />
-      </header>
-      {children}
-    </main>
-  )
-}
+import 'twin.macro'
+export { Container } from './Container'
+export { NextLink }
 
 interface BasicUIProps {
   className?: string
@@ -74,8 +47,6 @@ interface LinkProps {
   external?: boolean
   href: string
 }
-
-export { NextLink }
 
 export const Link: React.FC<LinkProps> = ({
   children,

807223734a71d8206a83b5899b928a6d972dcd99
Giacomo Rebonato <grebonato@outlook.ie>
2021-12-22 12:32:49 +0100

    check for full name

--- components/Container.tsx
+++ components/Container.tsx
@@ -10,7 +10,7 @@ import { Title } from '.'
 import { ColorModeSwitcher } from './color-mode-switcher'
 
 export const Container: React.FC<any> = ({ css, children }) => {
-  const [isLargerThan800] = useMediaQuery('(min-width: 800px)')
+  const [isLargerThan600] = useMediaQuery('(min-width: 600px)')
 
   return (
     <ChakraContainer maxW='container.md'>
@@ -18,7 +18,7 @@ export const Container: React.FC<any> = ({ css, children }) => {
         <HStack mb='4' pt='4' w='100%' alignItems='baseline'>
           <Title>
             <NextLink href='/'>
-              {isLargerThan800 ? 'Giacomo Rebonato' : 'Giacomo'}
+              {isLargerThan600 ? 'Giacomo Rebonato' : 'Giacomo'}
             </NextLink>
           </Title>
           <ColorModeSwitcher />
